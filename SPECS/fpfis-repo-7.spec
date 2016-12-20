2c7aa4b23318ef193bc1655b55bf92f3c66e01e8
Gregory Boddin <gregory@siwhine.net>
2016-12-20 22:55:44 +0100

    Initial import of older repo.
    - Updated docker scripts
    - Renamed to FPFIS

---
+++ SPECS/fpfis-repo-7.spec
@@ -0,0 +1,56 @@
+Name:           fpfis-repo
+Version:        6
+Release:        1%{?dist} 
+Summary:        FPFIS Packages for Enterprise Linux repository configuration
+
+Group:          System Environment/Base
+License:        GPLv2
+
+# This is a FPFIS maintained package which is specific to
+# our distribution.  Thus the source is only available from
+# within this srpm.
+
+URL:            https://github.com/ec-europa/fpfis-repo
+Source1:        fpfis-%{version}.repo
+Source2:        FPFIS-REPO-kEY 
+BuildArch:      noarch
+Requires:       redhat-release >=  l
+BuildRoot:      %{_tmppath}/%{name}-%{version}%{version}
+
+%description
+This package contains the FPFIS Packages for Enterprise Linux (EL) repository
+GPG key as well as configuration for yum.
+
+%prep
+%setup  -c -T
+
+%build
+
+
+%install
+rm -rf %{buildroot} 
+
+#GPG Key
+install -Dpm 644 %{SOURCE0} \
+    %{buildroot}%{_sysconfdir}/pki/rpm-gpg/FPFIS-REPO-KEY
+
+# yum
+install -dm 755 %{buildroot}%{_sysconfdir}/yum.repos.d
+install -pm 644 %{SOURCE1}  \
+    %{buildroot}%{_sysconfdir}/yum.repos.d/fpfis.repo
+
+%clean
+rm -rf %{buildroot} 
+
+%files
+%defattr(-,root,root,-)
+%config(noreplace) %{_sysconfdir}/yum.repos.d/*
+%{_sysconfdir}/pki/rpm-gpg/*
+
+%changelog
+* Tue Dec 20 2016 Gregory Boddin <gregory@siwhine.net> - 6-1
+- Created package for easy install on RHELs
+ 
+* Mon Dec 16 2013 Dennis Gilmore <dennis@ausil.us> - 6-0.1
+- initial epel 6 build. 
+
---
+++ build-docker.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+[ ! -x $(which docker) ] && echo "Docker must be installed" && exit 1
+
+set -e
+
+[ -z $1 ] && echo "Please specify a distro (6/7)"
+[ -z $2 ] && echo "Please specify a package (from the SPEC folder)"
+
+EL=$1
+DIST=el${EL}
+PACKAGE=$2
+CONTAINER=${PACKAGE}_${EL}_centos_build
+
+if [ "${PACKAGE}" == "master" ] || [ "${PACKAGE}" == "fpfis-repo" ]; then
+  PACKAGE="fpfis-repo"
+  SPECFILE="fpfis-repo-${EL}.spec"
+else
+  SPECFILE="${PACKAGE}.spec"
+fi
+
+echo "Using container name : ${CONTAINER}"
+
+# reset log files :
+cat /dev/null > RPMS/build.log
+cat /dev/null > RPMS/root.log
+cat /dev/null > RPMS/trace.log
+cat /dev/null > SRPMS/build.log
+cat /dev/null > SRPMS/root.log
+cat /dev/null > SRPMS/trace.log
+
+# Pull the image if missing :
+if [ -z $(docker images -q fpfis/mock) ]; then
+  echo "Building base mock image"
+  docker pull fpfis/mock
+fi
+
+# Create container if does not exists :
+if [ -z $(docker ps -qaf name=${CONTAINER}) ] ; then
+  docker create --privileged=true --name ${CONTAINER} -v $(pwd):/mock/build fpfis/mock bash -c 'tail -f /mock/build/{S,}RPMS/*.log'
+fi
+
+# Start container if not running 
+if [ "$(docker inspect -f {{.State.Running}}  ${CONTAINER})" != "true" ]; then
+    docker start ${CONTAINER} 
+    echo "Building container started : ${CONTAINER}"
+    echo "Use \"docker stop ${CONTAINER}\" when you're done working on this package"
+    sleep 1
+fi
+
+echo "Building ${PACKAGE}..."
+
+docker exec ${CONTAINER} spectool -g -C /mock/build/SOURCES/${PACKAGE} /mock/build/SPECS/${SPECFILE}
+docker exec -u ${UID} ${CONTAINER}  /usr/bin/mock -r fpfis-${EL}-x86_64 --spec=/mock/build/SPECS/${SPECFILE} --sources=/mock/build/SOURCES/${PACKAGE} --resultdir=/mock/build/SRPMS --buildsrpm
+docker exec -u ${UID} ${CONTAINER} /usr/bin/mock --clean -r fpfis-${EL}-x86_64  -D "dist .${DIST}" --resultdir=/mock/build/RPMS --rebuild /mock/build/SRPMS/$(ls -1utr SRPMS/|grep ^${PACKAGE}-.*src\.rpm$|head -1)
+docker stop ${CONTAINER} -f
