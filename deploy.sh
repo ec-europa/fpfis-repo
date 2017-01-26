1be39dab396ee4317bd4b6b2e8f2575191676503
Gregory Boddin <gregory@siwhine.net>
2017-01-26 12:49:58 +0100

    Batch mode

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ set +e
 [ -z ${RSYNC_PATH} ] && echo "RSYNC_PATH not set, aborting" && exit 1
 [ -z ${RSYNC_USER} ] && echo "RSYNC_USER not set, aborting" && exit 1
 
-rsync -avz -e "ssh -p ${RSYNC_PORT}" RPMS/ ${RSYNC_USER}@${RSYNC_HOST}:${RSYNC_PATH}
+rsync -avz -e "ssh -p ${RSYNC_PORT} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" RPMS/ ${RSYNC_USER}@${RSYNC_HOST}:${RSYNC_PATH}
