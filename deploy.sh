51f0cc0d8a2698390c8adcaa0b6e069a9c2ae93a
Gregory Boddin <gregory@siwhine.net>
2017-01-26 12:59:14 +0100

    Added key support

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ set +e
 [ -z ${RSYNC_PORT} ] && echo "RSYNC_PORT not set, aborting" && exit 1
 [ -z ${RSYNC_PATH} ] && echo "RSYNC_PATH not set, aborting" && exit 1
 [ -z ${RSYNC_USER} ] && echo "RSYNC_USER not set, aborting" && exit 1
-
-rsync -avz -e "ssh -p ${RSYNC_PORT} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" RPMS/ ${RSYNC_USER}@${RSYNC_HOST}:${RSYNC_PATH}
+[ -z ${RSYNC_KEY}  ] && echo "RSYNC_KEY not set, aborting" && exit 1
+rsync -avz -e "ssh -p ${RSYNC_PORT} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${RSYNC_KEY}" RPMS/ ${RSYNC_USER}@${RSYNC_HOST}:${RSYNC_PATH}
