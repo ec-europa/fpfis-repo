589d1001daf4025f2b4005deeca2d50c6db7e918
Rudi Van Houdt <rudi@iadept.be>
2017-02-14 10:34:20 +0100

    try again

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 set +e
+
 [ -z ${RSYNC_HOST} ] && echo "RSYNC_HOST not set, aborting" && exit 1
 [ -z ${RSYNC_PORT} ] && echo "RSYNC_PORT not set, aborting" && exit 1
 [ -z ${RSYNC_PATH} ] && echo "RSYNC_PATH not set, aborting" && exit 1
